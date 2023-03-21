35a848b1ecd71252e9b28ce4a373f90fe68bbec8
Basquiat <jlnmcneal@gmail.com>
2023-03-21 15:43:44 -0500

    Current day Container api call

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,12 @@
 // var input = $('input').val
 var button = $('button')
 var apiKey = '3bfb3e5fac26e7969dbe0245cdbbfe6c'
-
+var date = $('.date ')
+var temp = $('.temp')
+var humidity = $('.humidity')
+var wind = $('.windspeed')
+var uvIndex = $('.uv-index')
+var image = $('img')
 
 button.click(function(){
     console.log('jalen')
@@ -12,16 +17,27 @@ button.click(function(){
 
 var apiCall = function(e){
     var city = $('.form-input').val()
-    console.log(city)
     e.preventDefault();
-    const myUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-
+    const myUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
     fetch(myUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data)
+        var currentDay =  data.list[0]
+       
+        var icon = currentDay.weather[0].icon
+        var iconLink = 'http://openweathermap.org/img/w/' + icon + '.png'
+        date.text(data.city.name + ' ' +currentDay.dt_txt)
+        temp.text(currentDay.main.temp)
+        wind.text(currentDay.wind.speed + " mph")
+        humidity.text(currentDay.main.humidity + " %")
+        image.attr("src", iconLink)
+
+
+
+
     })
 
 } 
